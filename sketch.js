e0918f147325f4e855bbee3effec847e58e034de
ghool <santad06@yahoo.com>
2022-06-20 18:36:08 -0400

    Create grid function mostly working.

---
+++ sketch.js
@@ -0,0 +1,26 @@
+const container = document.querySelector(".container");
+
+
+function createGrid() {
+    for(let i = 0; i < 16; i++){
+        const sketchpadRow = createRow();
+        for(let j = 0; j < 16; j++) {
+            const sketchpad = createSketchpad (sketchpadRow);
+        }
+    }
+    
+    
+}
+function createRow () {
+    const sketchpadRow = document.createElement("div");
+    sketchpadRow.className = "sketchpadRow"
+    container.appendChild(sketchpadRow);
+    return sketchpadRow;
+}
+
+function createSketchpad (sketchpadRow) {
+    const sketchpad = document.createElement("div")
+    sketchpad.className = "sketchpad";
+    sketchpadRow.appendChild(sketchpad);
+    return sketchpad;
+}
