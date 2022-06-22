84afa8a0cb4cfecb71d238bf9dd905f31b2165dc
ghool <santad06@yahoo.com>
2022-06-21 23:41:14 -0400

    Add comments and update README Hover event only triggers once

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # TOP-Etch-a-Sketch
 Etch-a-Sketch project for TOP foundations section
+Some comments in the code are for myself as this is still a learning project, and I want to note important things I learned in code. because of this code may not be ideal.
+
+Fure plans/ideas maybe:
+
+-Add toggle for user to decide if they want color to change only once on mouse hover, or if they would want it to be on every hover?
+-Color selector (solid, random, black and white, etc.)
+-Better header/buttons/style overall
--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 const container = document.querySelector(".container");
 
 
-function createGrid(gridSize = 16) {
+function createGrid(gridSize = 16) { //herre we are defining grideSize even though it doesn't HAVE to be called that (we use it again later), just makes most sense. but 16 is essentially "defualt" on first load.
     for(let i = 0; i < gridSize; i++){
         const sketchpadRow = createRow();
         for(let j = 0; j < gridSize; j++) {
@@ -16,18 +16,18 @@ function createRow () {
     const sketchpadRow = document.createElement("div");
     sketchpadRow.className = "sketchpadRow"
     container.appendChild(sketchpadRow);
-    return sketchpadRow;
+    return sketchpadRow; //need to return this value since we are using this in other functions later and we need to return something.
 }
 
 function createSketchpad (sketchpadRow) {
     const sketchpad = document.createElement("div")
     sketchpad.className = "sketchpad";
     sketchpadRow.appendChild(sketchpad);
-    sketchpad.addEventListener("mouseover", sketch);
+    sketchpad.addEventListener("mouseover", sketch, {once: true}); // because we are calling sketch, a function we will define later, we don't use () or pass anything inside.
     return sketchpad;
 }
 
-function sketch (e) {
+function sketch (e) { //important here to put e or EVENT as the arguement, we are passing the mouseover event, essentially.
     let randomColor = Math.floor(Math.random()*16777215).toString(16);
     randomColor = "#" + randomColor;
     e.target.style.backgroundColor = randomColor
@@ -46,4 +46,4 @@ function newGrid() {
     createGrid(gridSize);
 };
 
-createGrid ()
+createGrid () //how to call a function and have it autorun on a page.
